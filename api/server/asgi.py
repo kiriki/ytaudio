17f403910a74c65aafc7349bd9c09793a9253381
Kirill Che <kiriciri@gmail.com>
2023-05-09 18:21:22 +0300

    consumer and routing

--- api/server/asgi.py
+++ api/server/asgi.py
@@ -5,8 +5,13 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 from django.core.asgi import get_asgi_application
+
+import server.apps.video_tasks.api.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
@@ -17,5 +22,10 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         'http': django_asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(server.apps.video_tasks.api.routing.websocket_urlpatterns),
+            ),
+        ),
     }
 )
